b4a45487024c4eaeaf517809e984e28c599a88b2
Kirill <belokopytovks2014@yandex.ru>
2021-11-02 10:59:47 +0300

    added onclick cretae game

--- client/src/components/Players/Players.jsx
+++ client/src/components/Players/Players.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSocket } from '../../contexts/socket.context';
 
 export default function Players({ player }) {
+  const { fetchSender, descriptors } = useSocket();
+
   return (
     <div>
       <li>
         <b>Логин:</b>&nbsp;{player.login}{' '}
-        <button type="button" className="btn btn-outline-primary btn-sm">
+        <button
+          onClick={(e) => {
+            fetchSender(descriptors.createGame(e.target.dataSet.id));
+          }}
+          data-id={player.id}
+          type="button"
+          className="btn btn-outline-primary btn-sm"
+        >
           Создать игру
         </button>
       </li>
