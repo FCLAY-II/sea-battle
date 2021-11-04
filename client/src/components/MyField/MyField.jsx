108b2c16f0d305548188040da8a5c031951e1852
Nikita <nik44226269@yandex.ru>
2021-11-04 19:13:22 +0300

    release field updated

--- client/src/components/MyField/MyField.jsx
+++ client/src/components/MyField/MyField.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-// import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import { useGameContext } from '../../contexts/game.context';
 import { useSocket } from '../../contexts/socket.context';
 import gameAC from '../../redux/actionCreators/gameAC';
@@ -16,9 +15,9 @@ export default function MyField() {
     1, 1, 1, 1
   ]);
 
-  const user = useSelector((state) => state.user);
+  
   const game = useSelector((state) => state.game);
-  const dispatch = useDispatch();
+
   const { makeField, putShip } = useGameContext();
   const currStateOfMyField = game.field;
   const field = makeField(currStateOfMyField);
@@ -26,11 +25,10 @@ export default function MyField() {
   // const [buttonState, setButtonState] = useState('visible');
   // const dispatch = useDispatch();
 
-  // const onDragEnd = result =>{
-  // };
+ 
 
   return (
-    // <DragDropContext onDragStart onDragUpdate onDragEnd={onDragEnd}>
+ 
 
     <div
       onClick={(e) => {
@@ -40,32 +38,20 @@ export default function MyField() {
       }}
 
       className="myside">
-      {/* <Droppable > */}
       <div id="myfield" className="field">
         {game.status === 'pending' ? <div>ждем, пока противник расставит свои корабли</div> : <></>}
         <h1>Моё поле</h1>
         {field.map((item) => item)}
       </div>
-      {/* </Droppable> */}
-      {/* <Ships /> */}
+     
       <Ships ships={ships} setShips={setShips} />
       {game.status === 'preparation' ? <button type="button"
         onClick={() => {
           fetchSender(descriptors.confirmShips(game.field));
           // setButtonState('unvisible');
         }}
-      >Готов к игре</button> : <></>}
-      { game.status === 'active' ? (
-        <div>
-          {game.currentPlayerId === user.id ? 
-            <p> сейчас ваш ход</p> 
-          : 
-            <p> ждем, пока сходит {game.enemy.login}</p>}
-        </div>
-      ) : (
-        null
-      )}
+      >Готов к игре</button> : null}
+    
     </div>
-    // </DragDropContext>
   );
 }
--- client/src/components/Row/Row.jsx
+++ client/src/components/Row/Row.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Row({ num, cells }) {
   return <div className="field-row">
-    <span style={{fontSize: '0.7rem', width: '20px'}}>{num}</span>
+    <span className='field-label'>{num}</span>
     {cells.map((item) => item)}
   </div>;
 }
